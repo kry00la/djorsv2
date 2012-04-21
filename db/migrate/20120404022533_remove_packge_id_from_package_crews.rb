11bad7ff36a1e59b7b35c2b6c0c4ab706816dbf2
LED <fullchaoz32@yahoo.com>
2012-04-21 23:32:10 +0800

    20120404022533_remove_packge_id_from_package_crews.rb removed

--- db/migrate/20120404022533_remove_packge_id_from_package_crews.rb
+++
@@ -1,9 +0,0 @@
-class RemovePackgeIdFromPackageCrews < ActiveRecord::Migration
-  def up
-    remove_column :package_crews, :packge_id
-      end
-
-  def down
-    add_column :package_crews, :packge_id, :integer
-  end
-end
