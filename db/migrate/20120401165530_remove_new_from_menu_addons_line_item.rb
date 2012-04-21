864c12c259e5be98b043e5ccfd183b76e79eb6f1
LED <fullchaoz32@yahoo.com>
2012-04-21 23:18:02 +0800

    removed unidentified migrations

--- db/migrate/20120401165530_remove_new_from_menu_addons_line_item.rb
+++
@@ -1,9 +0,0 @@
-class RemoveNewFromMenuAddonsLineItem < ActiveRecord::Migration
-  def up
-    remove_column :menu_addons_line_items, :new
-      end
-
-  def down
-    add_column :menu_addons_line_items, :new, :string
-  end
-end
--- db/migrate/20120401165548_remove_create_from_menu_addons_line_item.rb
+++
@@ -1,9 +0,0 @@
-class RemoveCreateFromMenuAddonsLineItem < ActiveRecord::Migration
-  def up
-    remove_column :menu_addons_line_items, :create
-      end
-
-  def down
-    add_column :menu_addons_line_items, :create, :string
-  end
-end
