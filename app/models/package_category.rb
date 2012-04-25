245f66a721b5154632ba0242c5283f98dd803a1a
LED <fullchaoz32@yahoo.com>
2012-04-26 00:36:29 +0800

    validations

--- app/models/package_category.rb
+++ app/models/package_category.rb
@@ -1,4 +1,5 @@
 class PackageCategory < ActiveRecord::Base
     validates :name, :description ,:presence => :true
+    validates :name, :uniqueness => :true
     has_many :package , :dependent => :destroy
 end
