cfe881d49c079e92cfcfcf506fb8c412a574b288
LED <fullchaoz32@yahoo.com>
2012-04-25 21:21:31 +0800

    validate_overlap update

--- app/models/function_room.rb
+++ app/models/function_room.rb
@@ -1,6 +1,7 @@
 class FunctionRoom < ActiveRecord::Base
-  has_many :reservation_function_rooms
+  has_many :reservation_function_rooms ,:dependent => :destroy
   has_attached_file :image_function_room, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_many :function_room_images ,:dependent => :destroy
   validates :name,:description,:price ,:presence => :true
   validates :name, :uniqueness => :true
 end
