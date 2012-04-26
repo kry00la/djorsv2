7283c0d52291f58663d084b887df13e232508032
LED <fullchaoz32@yahoo.com>
2012-04-27 04:46:41 +0800

    rollback

--- app/models/package_menu.rb
+++ app/models/package_menu.rb
@@ -2,5 +2,5 @@ class PackageMenu < ActiveRecord::Base
   belongs_to :menu
   belongs_to :package
   validates :menu, :package ,:presence => :true
-  validates :menu, :package, :uniqueness => :true
+
 end
