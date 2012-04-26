2301994c2b7ee3c2be22153f086f2ea62de3de90
LED <fullchaoz32@yahoo.com>
2012-04-27 04:39:11 +0800

    clearing

--- app/admin/package_menus.rb
+++ app/admin/package_menus.rb
@@ -19,16 +19,5 @@ ActiveAdmin.register PackageMenu do
   f.buttons
  end
  
-     controller do
-           def create
-                     @package = Package.find(params[:package_menu][:package_id])
-                      menu = Menu.find(params[:package_menu][:menu_id])
-                     @package_crew = @package.add_menu(menu.id)
-                         if @package_crew.save
-                             redirect_to {admin_package_crew_url}
-                         end                 
-                   end
-                  
-     end
-  
+ 
 end
