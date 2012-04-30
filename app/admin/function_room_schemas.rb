2a2be42bdbd0dcfced7f5bd21aa4e778ac734dee
LED <fullchaoz32@yahoo.com>
2012-04-30 21:35:35 +0800

    kabado sa schema

--- app/admin/function_room_schemas.rb
+++ app/admin/function_room_schemas.rb
@@ -2,13 +2,7 @@ ActiveAdmin.register FunctionRoomSchema do
    menu :label => "Function room schema" ,:parent => "Utilities"
    actions :all, :except => [:destroy]
 
-    index do 
-    column "schema",:schema do |schema|
-       image_tag(schema.schema.url(:cute))
-    end
    
-    default_actions
-    end
 
     form :html => { :enctype => "multipart/form-data" } do |f|
     f.inputs "Add Image to recipe" do
@@ -22,22 +16,8 @@ ActiveAdmin.register FunctionRoomSchema do
  
  
  
-  show do
-   panel "Details" do 
-     attributes_table_for function_room_schema do
-       row :created_at
-     end
-   end
-   active_admin_comments
- end
- 
- sidebar :Schema_image ,:only => :show do
-   attributes_table_for function_room_schema do
-     row :schema do 
-       image_tag(function_room_schema.schema.url(:prev))
-     end
-   end
- end
+  
+
  
  
  
